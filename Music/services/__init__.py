5d922ba83660a688ceb01d11699e1d11e3802a73
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-07 12:56:39 +0530

    Update __init__.py

--- Music/services/__init__.py
+++ Music/services/__init__.py
@@ -1,6 +1,21 @@
 from pyrogram import Client
+from os import listdir,
 
 from Music import config
+from Music.services.converter import convert
+from Music.services.downloaders import youtube
+from Music.services.queues import queues
+
+
+if "raw_files" not in listdir():
+    mkdir("raw_files")
+
+__all__ = ["convert"]
+
+__all__ = ["youtube"]
+
+__all__ = ["clear", "get", "is_empty", "put", "task_done"]
+
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 run = client.run
