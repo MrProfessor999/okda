da33a02656a45889f78cb04ef2fe601ca534e7a2
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-07 14:29:27 +0530

    Add files via upload

--- Music/modules/lyrics.py
+++ Music/modules/lyrics.py
@@ -1,41 +1,114 @@
-import io
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# MIT License
+# Copyright (c) 2020 Stɑrry Shivɑm // This file is part of AcuteBot
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-from pyrogram import Client as pbot
-from pyrogram import filters
-from tswift import Song
 
-# Lel, Didn't Get Time To Make New One So Used Plugin Made br @mrconfused and @sandy1709 dont edit credits
+import os, lyricsgenius
+
+from telegram.ext.dispatcher import run_async
+from telegram.ext import CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram.error import BadRequest
+from telegram import ForceReply
+
+from lunabot import dp, typing, LOG, GENIUS
+from lunabot.helpers import strings as st
+
+ARTIST, LYRICS = range(2)
+SONGDICT = {}
+
+if GENIUS is not None:
+    genius = lyricsgenius.Genius(GENIUS)
 
 
-@pbot.on_message(filters.command(["lyric", "lyrics"]))
-async def _(client, message):
-    lel = await message.reply("Searching For Lyrics.....")
-    query = message.text
-    if not query:
-        await lel.edit("`What I am Supposed to find `")
-        return
+@run_async
+@typing
+def songname(update, context):
+    update.effective_message.reply_text(
+        st.SONGNAME, reply_markup=ForceReply(selective=True)
+    )
 
-    song = ""
-    song = Song.find_song(query)
-    if song:
-        if song.lyrics:
-            reply = song.format()
-        else:
-            reply = "Couldn't find any lyrics for that song! try with artist name along with song if still doesnt work try `.glyrics`"
-    else:
-        reply = "lyrics not found! try with artist name along with song if still doesnt work try `.glyrics`"
+    return ARTIST
 
-    if len(reply) > 4095:
-        with io.BytesIO(str.encode(reply)) as out_file:
-            out_file.name = "lyrics.text"
-            await client.send_document(
-                message.chat.id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=query,
-                reply_to_msg_id=message.message_id,
+
+@run_async
+@typing
+def artistname(update, context):
+    msg = update.effective_message
+    user = update.effective_user
+    song = update.message.text
+
+    SONGDICT[user.id] = song
+    msg.reply_text(st.ARTISTNAME, reply_markup=ForceReply(selective=True))
+
+    return LYRICS
+
+
+@run_async
+@typing
+def sendlyrics(update, context):
+    chat = update.effective_chat
+    msg = update.effective_message
+    user = update.effective_user
+    artist = update.message.text
+
+    try:
+        song = SONGDICT[user.id]
+    except KeyError:
+        msg.reply_text(st.LYRICS_ERR)
+        return -1
+    rep = msg.reply_text("🔍 Looking for lyrics . . .")
+    lyrics = genius.search_song(song, artist)
+    if lyrics is None:
+        msg.reply_text(st.LYRIC_NOT_FOUND)
+        rep.delete()
+        return -1
+    try:
+        msg.reply_text(f"🎸 <b>{song}</b> by <b>{artist}</b>:\n\n{lyrics.lyrics}")
+
+    except BadRequest as excp:
+        if excp.message == "Message is too long":
+            msg.reply_text(st.LYRICS_TOO_BIG)
+            with open("acute-lyrics.txt", "w+") as f:
+                f.write(f"🎧 {song} by {artist}\n\n{lyrics.lyrics}")
+            context.bot.sendDocument(
+                chat_id=chat.id,
+                document=open("acute-lyrics.txt", "rb"),
+                caption=f"🎸 {song} - {artist}",
             )
-            await lel.delete()
-    else:
-        await lel.edit(reply)  # edit or reply
+            os.remove("acute-lyrics.txt")
+        else:
+            LOG.error(excp.message)
+
+    rep.delete()
+    del SONGDICT[user.id]
+    return -1
+
+
+@run_async
+@typing
+def cancel(update, context):
+    context.bot.sendMessage(update.effective_chat.id, (st.CANCEL))
+    return ConversationHandler.END
+
+
+LYRICS_HANDLER = ConversationHandler(
+    entry_points=[CommandHandler("lyrics", songname)],
+    states={
+        ARTIST: [MessageHandler(Filters.text & ~Filters.command, artistname)],
+        LYRICS: [MessageHandler(Filters.text & ~Filters.command, sendlyrics)],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+    conversation_timeout=120,
+)
+
+dp.add_handler(LYRICS_HANDLER)
