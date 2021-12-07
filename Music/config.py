e7a351faf31a61cf25fccbbb0f483530afe3adec
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-07 16:17:44 +0530

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -31,4 +31,4 @@ LOG_GRP = getenv("LOG_GRP", None)
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 TMDBAPI = getenv("TMDBAPI", None)
-dp = dispatcher
+
