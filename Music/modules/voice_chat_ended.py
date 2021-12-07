dc05fe30e14aba13471556adda3cf7992c9c67e0
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-07 13:24:06 +0530

    Update voice_chat_ended.py

--- Music/modules/voice_chat_ended.py
+++ Music/modules/voice_chat_ended.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from Music.helpers.channelmusic import get_chat_id
-from Music.services.callsmusic.callsmusic import remove
+from Music.services.callsmusic import remove
 
 
 @Client.on_message(filters.voice_chat_ended)
