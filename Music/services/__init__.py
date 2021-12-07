121d1e3cc8f9e9ed7aa34105ada8c5f8cbc20d01
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-07 20:15:14 +0530

    Update __init__.py

--- Music/services/__init__.py
+++ Music/services/__init__.py
@@ -7,14 +7,5 @@ from Music.services import youtube
 from Music.services import queues
 
 
-if "raw_files" not in listdir():
-    mkdir("raw_files")
-
-__all__ = ["convert"]
-
-__all__ = ["youtube"]
-__all__ = ["clear", "get", "is_empty", "put", "task_done"]
-
-
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 run = client.run
