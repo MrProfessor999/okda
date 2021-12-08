24c6d4498a728934a4dde62eac11ab2d38648cfd
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-08 12:49:13 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -2,7 +2,7 @@ import requests
 from pyrogram import Client as Bot
 
 from Music.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from Music.services import run
+from plugins import run
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
