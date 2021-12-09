34da655733f558a22ff8e227b394f89da84625f8
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-09 19:49:29 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,9 +1,11 @@
 import requests
-from pyrogram import Client as Bot
-
+from pyrogram import Client as bot
+from pyrogram import Client
 from Music.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from plugins import run
+from Music import config
 
+bot = bot(config.API_ID, config.API_HASH)
+run = bot.run
 bot = Bot(
     ":memory:",
     API_ID,
