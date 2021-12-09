078b182cebbba721f64436b4468d76d93bf73635
MrProfessor999 <91714877+MrProfessor999@users.noreply.github.com>
2021-12-09 14:02:19 +0530

    Update __main__.py

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,13 +1,15 @@
 import requests
 from pyrogram import Client as Bot
 
-from Music.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
+from Music.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN , BANNER, LOG
 from plugins import run
 
 response = requests.get(BG_IMAGE)
 file = open("./plugins/foreground.png", "wb")
 file.write(response.content)
 file.close()
+ 
+LOG.info("%s", BANNER)
 
 bot = Bot(
     ":memory:",
